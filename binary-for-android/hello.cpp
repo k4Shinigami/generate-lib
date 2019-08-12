04f52d0d7fd0f320e36e6fb8ba49371673aeb68a
sasideep bommareddy <sasideep.bommareddy@k4connect.com>
2019-08-12 12:43:18 -0400

    updated main hello.cpp for build

--- binary-for-android/hello.cpp
+++ binary-for-android/hello.cpp
@@ -1,12 +1,27 @@
 #include <android/log.h>
+// #include <ACL/include/ACL/AVSConnectionManager.h>
 
 #define LOG_TAG "your-log-tag"
+
 
 #define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, __VA_ARGS__)
 #define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
 
+class AlexaSample
+{
+     public:
+     AlexaSample() {
+          LOGD("got here");
+     };
+     void startAVS() {
+          LOGD("This should ideally start the AVS process");
+     };
+};
+
 int main()
 {
      LOGD("got here");
+     AlexaSample alexaClient;
+     alexaClient.startAVS();
      return 0;
 }

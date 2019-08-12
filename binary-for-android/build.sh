456750614d38f1fc25231cc9cd8e0e26033c48b5
sasideep bommareddy <sasideep.bommareddy@k4connect.com>
2019-08-12 10:33:38 -0400

    updated build scripts

--- binary-for-android/build.sh
+++ binary-for-android/build.sh
@@ -1,12 +1,6 @@
-export NDK_ROOT=${ANDROID_NDK_ROOT}
+# delete the previous generated files
+rm -f hello-binary
+rm -f hello-compiled.cpp.o
 
-${NDK_ROOT}/toolchains/llvm/prebuilt/darwin-x86_64/bin/clang++ \
-     --target=aarch64-linux-android23 \
-     --gcc-toolchain=${NDK_ROOT}/toolchains/aarch64-linux-android-4.9/prebuilt/darwin-x86_64  \
-     -isystem ${NDK_ROOT}/sysroot/usr/include/aarch64-linux-android \
-     -o  hello-compiled.cpp.o -c hello.cpp
-
-${NDK_ROOT}/toolchains/llvm/prebuilt/darwin-x86_64/bin/clang++ \
-     --target=aarch64-linux-android23 \
-     --gcc-toolchain=${NDK_ROOT}/toolchains/aarch64-linux-android-4.9/prebuilt/darwin-x86_64 \
-     -isystem ${NDK_ROOT}/platforms/android-27/arch-arm  hello-compiled.cpp.o -o hello-binary
+# run the 32bit compilation
+./build-for-32bit.sh

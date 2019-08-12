22d26fd935e8e0c11b0a57beccf5002ffc32744a
sasideep bommareddy <sasideep.bommareddy@k4connect.com>
2019-08-12 12:42:15 -0400

    updated simple cpp to use class

--- simple-cpp/static-check.cpp
+++ simple-cpp/static-check.cpp
@@ -1,7 +1,23 @@
-#include <iostream>
+// #include <ACL/include/ACL/AVSConnectionManager.h>
+#include<iostream>
+
+#define LOG_TAG "your-log-tag"
+
+class AlexaSample
+{
+     public:
+     AlexaSample() {
+          printf("got here");
+     };
+     void startAVS() {
+          printf("This should ideally start the AVS process");
+     };
+};
 
 int main()
 {
-     std::cout << "Hello there";
+     printf("got here");
+     AlexaSample alexaClient;
+     alexaClient.startAVS();
      return 0;
 }

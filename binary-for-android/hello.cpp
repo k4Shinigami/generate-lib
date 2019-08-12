7d8cf78c1ea5c12bf1e74a48492e797ef00f37dc
sasideep bommareddy <sasideep.bommareddy@k4connect.com>
2019-08-12 09:48:20 -0400

    android doesn't ve header fr iostream - used log

--- binary-for-android/hello.cpp
+++ binary-for-android/hello.cpp
@@ -1,8 +1,12 @@
-#include <iostream>
+#include <android/log.h>
+
+#define LOG_TAG "your-log-tag"
+
+#define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, __VA_ARGS__)
+#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
 
 int main()
 {
-     std::cout << "Hello there";
+     LOGD("got here");
      return 0;
 }
-
